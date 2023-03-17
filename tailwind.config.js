32f83c5c7df19ecd25d9fba36f7fea1cbfb8e2ec
Gradon <123156149+dinosoft-web@users.noreply.github.com>
2023-03-17 16:23:47 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./*.html'],
+	theme: {
+		screens: {
+			sm: '480px',
+			md: '768px',
+			lg: '1020px',
+			xl: '1440px',
+		},
+		extend: {
+			colors: {
+				softBlue: '#3662F7',
+				softRed: '#940CAB',
+				grayishBlue: 'hsl(229, 8%, 60%)',
+				veryDarkBlue: '#2E4AAB',
+			},
+			fontFamily: {
+				sans: ['Rubik', 'sans-serif'],
+			},
+			backgroundImage: () => ({
+				dots: "url('../images/bg-dots.svg')",
+			}),
+		},
+	},
+	plugins: [],
+}
